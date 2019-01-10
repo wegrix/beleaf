5b3e10403b7e7b07681a3c9eddb209f2e7b5edf6
wegrix <contact@wegrix.com>
2019-01-10 15:09:14 -0500

    Making app serve real directory

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/beleaf'));
+app.use(express.static('./dist/beleafdesign'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/beleaf/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/beleafdesign/index.html'));
 });
 
 // Start App
