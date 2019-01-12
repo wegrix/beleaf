6dde7a3a56ca9b833566b566ee262182b71417b9
wegrix <contact@wegrix.com>
2019-01-12 16:16:26 -0500

    Added For loop for Social Media

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  instagrams: any = [
+      {
+      url: 'https://www.instagram.com/beleaf_design/',
+      name: '#naturefriendlydesign'
+    },
+    {
+      url: 'https://www.instagram.com/beleaf_design/',
+      name: '#ecofriendlydesign'
+    },
+    {
+      url: 'https://www.instagram.com/beleaf_design/',
+      name: '#localdesign'
+    },
+    {
+      url: 'https://www.instagram.com/beleaf_design/',
+      name: '#beleafdesign'
+    }
+  ];
 
   constructor() { }
 
