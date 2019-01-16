a2f2c2b16c125bd2034dfa7898a4848eac2f79a5
wegrix <contact@wegrix.com>
2019-01-15 21:46:07 -0500

    Added minification middleware

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mollify = require('mollify');
 const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/beleafdesign'));
+
+// Minify JS and CSS files
+app.use(mollify({
+  dir: './dist/beleafdesign'
+}))
+
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/beleafdesign/index.html'));
